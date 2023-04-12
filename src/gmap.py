9d50b7bb7a75e6372ff5f918a9507098fac312e1
archmans <13521010@std.stei.itb.ac.id>
2023-04-12 07:51:33 +0700

    delete: gmap

--- src/gmap.py
+++
@@ -1,6 +0,0 @@
-# import gmplot
-
-# # Create the map plotter:
-# apikey = 'AIzaSyB5UAh67qqEWkt8i2VH6AMD3KJgIdx4vNI' # (your API key here)
-# gmap = gmplot.GoogleMapPlotter(37.766956, -122.448481, 14, apikey=apikey)
-# gmap.draw('map.html')
